aeb4d7431b3b0264efda75ff279624e43a36a72a
dlacuey <lacuey.david@gmail.com>
2025-09-14 14:50:25 +0200

    fini exo 4

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,31 +1,35 @@
 import sys
 
+
 def mylen(seq):
     count = 0
     for item in seq:
         count += 1
     return count
 
+
 def clean_arg(s):
     start = 0
     end = mylen(s) - 1
-    while start <= end and s[start] == ' ':
+    while start <= end and s[start] == " ":
         start += 1
-    while end >= start and s[end] == ' ':
+    while end >= start and s[end] == " ":
         end -= 1
-    return s[start:end+1]
+    return s[start: end + 1]
+
 
 def is_valid_integer(s):
     if mylen(s) == 0:
         return False
-    if s[0] in ('-', '+'):
+    if s[0] in ("-", "+"):
         if mylen(s) == 1:
             return False
         s = s[1:]
     for c in s:
-        if not ('0' <= c <= '9'):
+        if not ("0" <= c <= "9"):
             return False
     return True
+
 
 def find_number(c):
     digits = "0123456789"
@@ -36,19 +40,21 @@ def find_number(c):
         i += 1
     return -1
 
+
 def my_atoi(s):
     sign = 1
     i = 0
-    if s[0] == '-':
+    if s[0] == "-":
         sign = -1
         i = 1
-    elif s[0] == '+':
+    elif s[0] == "+":
         i = 1
     num = 0
     while i < mylen(s):
         num = num * 10 + find_number(s[i])
         i += 1
     return sign * num
+
 
 def main():
     seq = sys.argv
@@ -60,9 +66,10 @@ def main():
         print("AssertionError: argument is not an integer")
         return
     result = my_atoi(arg)
-    if (result % 2 == 0):
+    if result % 2 == 0:
         print("I'm Odd.")
     else:
         print("I'm Even.")
+
 
 main()
